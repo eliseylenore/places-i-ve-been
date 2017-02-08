91da697dd74cc0c61015e3e93baa61ed033093b2
Elise St Hilaire and Sandro Mijatovic <elise+sandro@>
2017-02-08 11:31:00 -0800

    create our allplaces html and connect it to homemodule, but will have and error

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -9,11 +9,17 @@ namespace Places
   {
     public HomeModule()
     {
+      Get["/all_places"] = _ => {
+        List<string> cityNames = Place.GetAll();
+        View["all_places.cshtml", cityNames];
+      };
         Get["/"] = _ => View["index.cshtml"];
         Post["/place_created"] = _ => {
           Place newPlace = new Place(Request.Form["new-city"]);
+          newPlace.Save();
           return View["place_created.cshtml", newPlace];
         };
+
     }
   }
 }
--- Objects/Places.cs
+++ Objects/Places.cs
@@ -6,6 +6,7 @@ namespace Places.Objects
   public class Place
   {
     private string _city;
+    private static List<string> _cityList = new List<string>();
 
     public Place (string city)
     {
@@ -21,5 +22,15 @@ namespace Places.Objects
     {
       return _city;
     }
+
+    public static List<string> GetAll()
+    {
+      return _cityList;
+    }
+    public void Save()
+    {
+      _cityList.Add(_city);
+    }
+
   }
 }
